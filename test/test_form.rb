6e6a03d4a38240a48b63ff6558b8c4d8b4051796
Andy Waite <andy@andywaite.com>
2012-03-28 10:26:05 +0100

    Adding test for password fields

--- test/test_form.rb
+++ test/test_form.rb
@@ -2,31 +2,36 @@ require File.dirname(__FILE__) + '/helper'
 require 'capybara'
 
 class TestForm < Test::Unit::TestCase
-  context "" do
+  context "Form" do
     setup do
       @form = load_fixture(CapybaraPageObject::Form)
     end
 
-    should "buttons" do
-      assert_equal 4, @form.buttons.size
+    context "#buttons" do
+      should "return the buttons contained in the form" do
+        assert_equal 4, @form.buttons.size
+      end
     end
 
-    should "fields" do
-      f = {
-        'input_1' => 'value_1',
-        'input_2' => 'value_2',
-        'colour' => 'blue',
-        'essay' => 'hello world',
-        'checkbox_1' => false,
-        'checkbox_2' => true,
-        'radio_button_1' => false,
-        'radio_button_2' => true,
-        'countries' => ['spain', 'germany']
-      }
-      assert_equal f, @form.fields
+    context "#fields" do
+      should "return the fields contained in the form" do
+        f = {
+          'input_1' => 'value_1',
+          'text_input' => 'text_input_value',
+          'password_input' => 'password',
+          'colour' => 'blue',
+          'essay' => 'hello world',
+          'checkbox_1' => false,
+          'checkbox_2' => true,
+          'radio_button_1' => false,
+          'radio_button_2' => true,
+          'countries' => ['spain', 'germany']
+        }
+        assert_equal f, @form.fields
+      end
     end
 
-    should "field access" do
+    should "provide direct access to fields" do
       assert_equal 'hello world', @form.essay
     end
   end
