c78c96c5eedd4f3820224c28f89ad456da354f03
Andy Waite <andy@andywaite.com>
2012-03-31 15:15:27 +0100

    Refactor to shorter methods

--- lib/element/form.rb
+++ lib/element/form.rb
@@ -6,20 +6,9 @@ module CapybaraPageObject
 
     def fields
       r = {}
-      all('input').each do |input|
-        if CHECKABLE.include?(input[:type])
-          r[input[:name]] = !!input.checked?
-        elsif !BUTTON_TYPES.include?(input[:type])
-          r[input[:name]] = input.value
-        end
-      end
-      all('select').each do |select|
-        r[select[:name]] = select.value
-      end
-      all('textarea').each do |textarea|
-        r[textarea[:name]] = textarea.value
-      end
-      r
+      r.merge! inputs
+      r.merge! selects
+      r.merge! textareas
     end
 
     def buttons
@@ -41,6 +30,32 @@ module CapybaraPageObject
     def method_missing(sym, *args, &block)
       return fields[sym.to_s] if fields.has_key?(sym.to_s)
       super(sym, *args, &block)
+    end
+    
+    private
+    
+    def inputs
+      r = {}
+      all('input').each do |input|
+        if CHECKABLE.include?(input[:type])
+          r[input[:name]] = !!input.checked?
+        elsif !BUTTON_TYPES.include?(input[:type])
+          r[input[:name]] = input.value
+        end
+      end
+      r
+    end
+    
+    def textareas
+      all('textarea').inject({}) do |result, textarea|
+        result.merge textarea[:name] => textarea.value
+      end
+    end
+    
+    def selects
+      all('select').inject({}) do |result, select|
+        result.merge select[:name] => select.value
+      end
     end
   end
 end
