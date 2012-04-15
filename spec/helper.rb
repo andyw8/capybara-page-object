576e2499ff82184975af53e5d14dc318cea1acc3
Andy Waite <github.aw@andywaite.com>
2012-04-15 12:19:12 +0100

    Configuring SimpleCov (skip on Travis)

--- spec/helper.rb
+++ spec/helper.rb
@@ -12,4 +12,8 @@ require 'rspec'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
+
+require 'simplecov'
+SimpleCov.start unless ENV["SKIP_COVERAGE"]
+
 require 'capybara-page-object'
